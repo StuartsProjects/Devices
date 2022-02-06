cff7dec2fbf4e9a24e2479a5b8cc5c9108b113bd
StuartsProjects <stuart [at] stuartsprojects.com>
2022-02-06 17:20:05 +0000

    Add board for ESP32CAM

---
+++ Long Range Wireless Adapter for ESP32CAM V2/2022-02-05-Long-Range-Wireless-Adapter-for-ESP32CAM Tindie.md
@@ -0,0 +1,74 @@
+## Long Range Wireless Adapter for ESP32CAM with LoRa
+
+This is an adapter board for the ESP32CAM with a fitted DRF1278F 434Mhz LoRa module.
+
+The LoRa module will allow the transfer of the images taken by the ESP32CAM over very much longer distances that would be possible with WiFi or Bluetooth. With a second Long Range Wireless Adapter board acting as the receiver the images can be automatically saved to an SD card or transferred via a serial link to a folder on a PC. 
+
+The ESP32CAM is not provided.
+
+The assembled board is shown below. There are some choices in how the board is built so soldering the pin headers, sockets, wires and antenna is left to the user. 
+
+<br>
+<p align="center">
+  <img width="300"  src="/ESP32CAM_29.jpg">
+</p>
+<br>
+
+<br>
+<p align="center">
+  <img width="450"  src="/ESP32CAM_28.jpg">
+</p>
+<br>
+
+### Partial kit for Long Range Wireless Adapter board
+
+Supplied with the board is enough parts to make a functional Long Range Wireless Adapter board. You will need to solder several components into place, using your your own solder. The DRF1278F LoRa module is already soldered in place on the board. The kit contains;
+
+- PCB with fitted DRF1278F LoRa module
+- 2 x 8 pin 0.1" header sockets
+- 4 pin 0.1" pin header for programming connection
+- 3 pin 0.1" pin header for Serial2 monitor connection
+- IN5817 Diode
+- JST battery lead
+- Tie wrap for battery lead
+- SMA antenna socket
+- Antenna wire, 17cm
+- Pin 33 wire, silicone.
+- Boot switch
+
+
+<br>
+<p align="center">
+  <img width="450"  src="/ESP32CAM_30.jpg">
+</p>
+<br>
+
+You will need to remove the white LED (or its driver transistor) that is on GPIO4 as this pin is needed for the LoRa device. See component circled in red below;
+
+<br>
+<p align="center">
+  <img width="250"  src="/ESP32CAM_GPIO4_Transistor.jpg">
+</p>
+<br>
+
+There are Arduino programs provided that allow the assembled board to take pictures, save them to the SD card on the ESP32CAM and then transfer the image via LoRa to another remote Arduino acting as the receiver. The receiver can be another Long Range Wireless Adapter and ESP32CAM board or another Arduino. You will need to download an install a specific Arduino LoRa library.
+
+There is an optional program provided that allows the receiver Arduino to transfer the received images across to a folder on a Windows PC using a serial link and the terminal program Tera Term.
+
+Example programs are provided for carrying out basic tests on the assembled board, see the documentation link below. 
+
+### Other optional components
+
+The ESP32CAM is fitted with an AMS1117 regulator this has a fairly large drop-out voltage making operation from 3 x AAs or a single Lithium battery problematic. To resolve this situation the AMS1117 can be removed and an MCP1702 or HT7833 regulator fitted on the Long Range Wireless Adapter, see the build instructions for details.
+
+There is a connector on the bottom of the board that can be used for a GPS which uses the Serial programming port.
+
+Resistors R1 and R2 can be fitted and allows for reading the battery voltage. This might be useful if your also trying to use the Long Range Wireless Adapter as a GPS tracker with the GPS fitted to the 4 pin connector at the bottom of the PCB. 
+
+SW1 is a small switch that can e fitted which will disconnect the GPS, if fitted, temporarily from the UOR\RXD pin to allow program uploads to be carried out.
+
+
+
+### Stuart Robinson
+
+### February 2022
---
+++ Long Range Wireless Adapter for ESP32CAM V2/5_ESP32CAM_PCFileTransfer_Test/5_ESP32CAM_PCFileTransfer_Test.ino
@@ -0,0 +1,135 @@
+/*******************************************************************************************************
+  Programs for Arduino - Copyright of the author Stuart Robinson - 03/02/22
+
+  This program is supplied as is, it is up to the user of the program to decide if the program is
+  suitable for the intended purpose and free from errors.
+*******************************************************************************************************/
+
+/*******************************************************************************************************
+  Program Operation - This is a test program to allow you to check if the YModem link to a PC is setup 
+  and working correctly.
+
+  The program creates an array in PSRAM and then fills it with 8 'lines' of ASCII chatacters from 0x20 to
+  0x7E. If Tera Term on the PC is setup correctly the resulting file should appear in the selected PC 
+  folder, it is creates as a text file and can be viewed in Notepad. The lines of the transferred file
+  should look like this;
+  
+   !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ 
+
+  Progress and monitor messages are set to the Serial2 TX pin which is set at pin 33, the Red LED pin. 
+  A wire link needs to be added to connect the Serial2 output connector, CONA on the ESP32CAM Long Range
+  Wireless Adapter PCB.
+  
+  Serial monitor baud rate is set at 115200.
+*******************************************************************************************************/
+
+#include <SPI.h>
+
+
+#define REDLED 33                          //pin number for ESP32CAM on board red LED, set logic level low for on
+#define RXD2 40                            //RX pin for Serial2 monitor port, not used 
+#define TXD2 33                            //TX pin for Serial2 monitor port
+
+const uint32_t ARDTarraysize = 0x20000;    //maximum file\array size to receive
+
+
+uint8_t *PSRAMptr;                         //create a global pointer to the array to send, so all functions have access
+char ARDTfilenamebuff[] = "TestFile.txt";
+uint32_t arraylength;
+uint32_t bytestransfered;
+
+#include "YModemArray.h"                   //YModem transferring array functions, local file
+
+
+void loop()
+{
+  Serial2.println(F("YModem file transfer starting"));
+  Serial2.flush();
+  led_Flash(1,1000);
+  bytestransfered = yModemSend(ARDTfilenamebuff, PSRAMptr, arraylength, 1, 1);
+  Serial2.flush();
+  led_Flash(1,1000);
+
+  if (bytestransfered == arraylength)
+  {
+  Serial2.println(F("YModem transfer OK"));
+  }
+  else
+  {
+  Serial2.println(F("YModem transfer FAILED"));
+  }
+  Serial2.println();
+  Serial2.flush();
+  delay(10000);
+}
+
+
+void led_Flash(uint16_t flashes, uint16_t delaymS)
+{
+  uint16_t index;
+  for (index = 1; index <= flashes; index++)
+  {
+    digitalWrite(REDLED, HIGH);
+    delay(delaymS);
+    digitalWrite(REDLED, LOW);
+    delay(delaymS);
+  }
+}
+
+
+void setup()
+{
+  uint32_t available_PSRAM_size;
+  uint32_t new_available_PSRAM_size;
+  uint8_t index1, index2;
+
+  pinMode(REDLED, OUTPUT);                            //setup pin as output for indicator LED
+  
+  Serial.begin(115200);                               //Ymodem transfer port
+
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);      //debug port, format is Monitorport.begin(baud-rate, protocol, RX pin, TX pin)
+  Serial2.println();
+  Serial2.println(__FILE__);
+  Serial2.println();
+  Serial2.println();
+
+  if (psramInit())
+  {
+    Serial2.println("PSRAM is correctly initialised");
+    available_PSRAM_size = ESP.getFreePsram();
+    Serial2.println((String)"PSRAM Size available: " + available_PSRAM_size);
+  }
+  else
+  {
+    Serial2.println("PSRAM not available");
+    while (1);
+  }
+
+  Serial2.println("Allocate array in PSRAM");
+  uint8_t *byte_array = (uint8_t *) ps_malloc(ARDTarraysize * sizeof(uint8_t));
+  PSRAMptr = byte_array;                              //save the pointe to byte_array to global pointer
+
+  new_available_PSRAM_size = ESP.getFreePsram();
+  Serial2.print("PSRAM Size available: ");
+  Serial2.println(new_available_PSRAM_size);
+  Serial2.print("PSRAM array bytes allocated: ");
+  Serial2.println(available_PSRAM_size - new_available_PSRAM_size);
+
+  //fill array with test data
+  arraylength =0;
+
+  for (index1 = 0; index1 <= 8; index1++)
+  {
+   for (index2 = 0x20; index2 <= 0x7E; index2++)
+   {
+   PSRAMptr[arraylength] = index2;
+   arraylength++;
+   }
+   PSRAMptr[arraylength++] = 0x0d;
+   PSRAMptr[arraylength++] = 0x0a;
+  }
+
+  Serial2.print("Array bytes written ");
+  Serial2.println(arraylength);
+  Serial2.println();
+}
